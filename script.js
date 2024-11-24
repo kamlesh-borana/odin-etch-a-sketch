d347674741da2a2ad909ef6caa732849988ea8e9
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-24 18:58:47 +0530

    Attach event listener on grid item for hover
    Attached mouseenter event listener on grid item to add class to it on
    hover to apply styles on it.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const gridContainer = document.querySelector(".grid-container");
 
+function attachEventListener(element) {
+    element.addEventListener("mouseenter", () => {
+        element.classList.add("hovered");
+    });
+}
+
 function createGrid(row, column = row) {
     for (let i = 1; i <= row; i++) {
         const gridRow = document.createElement("div");
@@ -8,6 +14,7 @@ function createGrid(row, column = row) {
         for (let j = 1; j <= column; j++) {
             const gridItem = document.createElement("div");
             gridItem.classList.add("grid-item");
+            attachEventListener(gridItem);
             gridRow.appendChild(gridItem);
         }
 

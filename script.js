a7a90514941a8459dbd82b5786b559bd95209e57
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-24 18:36:37 +0530

    Create and add grid to the UI
    Added a function which programmatically adds a grid ui using the passed
    number of row and column and applied style to it.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Project Etch-a-Sketch");
+const gridContainer = document.querySelector(".grid-container");
+
+function createGrid(row, column = row) {
+    for (let i = 1; i <= row; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+        
+        for (let j = 1; j <= column; j++) {
+            const gridItem = document.createElement("div");
+            gridItem.classList.add("grid-item");
+            gridRow.appendChild(gridItem);
+        }
+
+        gridContainer.appendChild(gridRow);
+    }
+}
+
+createGrid(16);
